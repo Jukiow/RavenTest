81a6ea6d319bfc1cad666c8b7532da8ea74cfbfa
Jukiow <32683554+Jukiow@users.noreply.github.com>
2024-03-24 15:26:18 +0000

    [RavenTrial] - Q1

--- forgottenserver-1.4.2/data/creaturescripts/scripts/logout.lua
+++ forgottenserver-1.4.2/data/creaturescripts/scripts/logout.lua
@@ -1,7 +1,18 @@
+local function releaseStorage(player)
+    -- Set storage value to -1 to release storage
+    player:setStorageValue(1000, -1)
+end
+
+
 function onLogout(player)
 	local playerId = player:getId()
 	if nextUseStaminaTime[playerId] then
 		nextUseStaminaTime[playerId] = nil
 	end
+
+	if player:getStorageValue(1000) == 1 then
+        releaseStorage(player)  -- Call releaseStorage immediately instead of scheduling an event
+    end
+
 	return true
 end
